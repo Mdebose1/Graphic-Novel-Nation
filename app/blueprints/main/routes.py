c39e245c1dc78658b16952d8055a2fcc234a3850
Mikkia DeBose <kiabo1995@gmail.com>
2022-06-06 16:37:57 -0500

    set up login/registration

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,14 +1,15 @@
-import imp
 from flask import render_template, current_app as app, request, redirect, url_for, flash
 from datetime import datetime as dt
 from app.blueprints.posts.models import Post
 from app import db
+from flask_login import current_user
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print(current_user)
     if request.method =='POST':
         data = request.form.get('posts')
-        p = Post(body=data, author='1ad38e4632eb450fbe9b2206fa9f10e5')
+        p = Post(body=data, author=current_user.get_id())
         db.session.add(p)
         db.session.commit()
 
@@ -24,10 +25,15 @@ def popular():
 def genre():
     return 'Genre'
 
-@app.route('/logout')
-def logout():
-    return 'Logout'
 
-@app.route('/register')
-def register():
-    return 'Register'
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if request.method =='POST':
+        data = request.form.get('posts')
+        p = Post(body=data, author=current_user.get_id())
+        db.session.add(p)
+        db.session.commit()
+        flash('Nice. Your post has been created!')
+        return  redirect(url_for('profile'))
+    return render_template('main/home.html', posts=[post.to_dict() for post in Post.query.filter_by(author=current_user.get_id()).order_by(Post.date_created.desc()).all()])
+    
--- app/blueprints/posts/models.py
+++ app/blueprints/posts/models.py
@@ -1,8 +1,13 @@
 from enum import unique
+from tabnanny import check
 import uuid
 from datetime import datetime as dt
 from app import db
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String(32), primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
@@ -10,6 +15,12 @@ class User(db.Model):
     password = db.Column(db.String(300))
     posts = db.relationship('Post', backref='posts', cascade='all, delete-orphan')
 
+    def generate_password(self, password_from_form):
+        self.password = generate_password_hash(password_from_form)
+    
+    def check_password(self, password_from_form):
+        return check_password_hash(self.password, password_from_form)
+        
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.id = uuid.uuid4().hex
@@ -38,3 +49,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post: {self.body[30]}...>'
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
