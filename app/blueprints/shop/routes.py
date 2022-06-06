c1ea629ebf96f9318c1348faf7b3d53276a4d25b
Mikkia DeBose <kiabo1995@gmail.com>
2022-06-06 18:08:29 -0500

    products added

--- app/blueprints/shop/routes.py
+++ app/blueprints/shop/routes.py
@@ -1,8 +1,23 @@
-from flask import render_template, current_app as app
+from math import prod
+from flask import current_app as app, render_template
+import stripe
 
-@app.route('/shop')
-def shop():
-    return 'Shop'
+stripe.api_key = "sk_test_51L7nhTKW2PHafdlZ2JG2Qs0B1s93zhy5dxHQTIIwkl11s1qHWLQgpn4nzIDUeXBuApzW1fq5GBZ0snR8Lj9r9Vl500SDgU5KmF"
+
+
+@app.route('/items')
+def items():
+    products = []
+    for product in stripe.Product.list()['data']:
+        product_dict = {
+            'id': product['id'],
+            'name': product['name'],
+            'description': product['description'] ,
+            'price':stripe.Price.retrieve(product['default_price']) ['unit_amount'] /100,
+            'image': product['images'][0],
+        }
+        products.append(product_dict)
+    return render_template('shop/items.html', products=products )
 
 @app.route('/checkout')
 def checkout():
